53ed891001a93f2e0f8724a34bdaece575c531c6
Instituto Milenio BASE <130472788+instituto-base@users.noreply.github.com>
2024-01-24 12:15:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const groupID = '5360829'; // Puedes usar tu ID de usuario si es una biblioteca personal
 const zoteroDataDiv = document.getElementById('zotero-data');
 
-fetch(`https://api.zotero.org/groups/${groupID}/items/top?format=json&sort=date`)
+fetch(`https://api.zotero.org/groups/${groupID}/items/top?direction=desc&format=json&sort=dateAdded`)
   .then(response => response.json())
   .then(data => {
     displayZoteroData(data);
